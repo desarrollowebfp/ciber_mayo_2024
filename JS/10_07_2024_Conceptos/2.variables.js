e5be61557b1ae68e5211330465e4e2b95352d4ff
Antonio Rosales <desarrollowebfp@thepower.education>
2024-07-10 21:43:51 +0200

    js

---
+++ JS/10_07_2024_Conceptos/2.variables.js
@@ -0,0 +1,7 @@
+//Variables
+let firstName = "Peter";
+
+firstName = "Byron";
+console.log(firstName);
+//Constantes
+const lastName = "Hades";
---
+++ JS/10_07_2024_Conceptos/3.tiposDeDatos.js
@@ -0,0 +1,28 @@
+//Strings
+const string = "Alberto dijo: 'A programar se aprende programando'";
+const string2 = "Hola que tal $%/%$&(";
+const string3 = ``;
+
+//Number
+const number = 10.5;
+
+//Booleans
+const boolean = false;
+
+//Arrays
+const animales = ["Perro", "Gato", "Pato", "Paloma"];
+
+//Objetos
+const perro = {
+  name: "Byron",
+  age: 4,
+  color: "Multicolor",
+  weight: 25,
+  tricks: ["Sit", "Eat"],
+};
+
+//Null
+const nulo = null;
+
+//Undefined
+const noDefinido = undefined;
